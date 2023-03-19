7777617c38e46b752d59824e897e30d9aadc220b
Claudio Lins <claudio.lins@me.com>
2023-03-19 21:27:16 +0000

    Update page.tsx

--- src/app/summary/page.tsx
+++ src/app/summary/page.tsx
@@ -13,7 +13,17 @@ export default async function Sumary() {
 
   return (
     <div className="w-full backdrop-blur-sm max-w-6xl flex justify-center h-screen rounded-md bg-white/50 p-4">
-      <Summary entries={entries} />
+      {/* <Summary entries={entries} /> */}
+      {
+        entries.map((entry: any) => {
+          return (
+            <div key={entry.id}>
+              <h1>{entry.title}</h1>
+              <p>{entry.amount}</p>
+            </div>
+          );
+        })
+      }
 
     </div>
   );
