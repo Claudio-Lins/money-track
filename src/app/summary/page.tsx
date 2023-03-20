06a95a8555dc44f4d709296bd5a6eeb13a29f7d8
Claudio Lins <claudio.lins@me.com>
2023-03-20 16:19:04 +0000

    Update page.tsx

--- src/app/summary/page.tsx
+++ src/app/summary/page.tsx
@@ -1,6 +1,8 @@
 import { Summary } from "@/components/Summary";
 import React from "react";
 
+export const revalidate = 10;
+
 async function getEntries() {
   const response = await fetch(`${process.env.BASE_URL}/api/entries`,{
     next: {
@@ -10,20 +12,12 @@ async function getEntries() {
   return response.json();
 }
 
-
-
 export default async function Sumary() {
-
-  
   const entries = await getEntries();
 
   return (
     <div className="w-full backdrop-blur-sm max-w-6xl flex justify-center h-[calc(100vh_-_100px)] md:h-[calc(100vh_-_150px)] overflow-auto rounded-md bg-white/50 p-4">
       <Summary entries={entries} />
-      
-    {/* <pre>
-      {JSON.stringify(entries, null, 2)}
-    </pre> */}
     </div>
   );
 }
