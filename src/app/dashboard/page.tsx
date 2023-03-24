ea545b1bdb35dd064732d19926b28a9e1815573d
Claudio Lins <claudio.lins@me.com>
2023-03-24 18:44:35 +0000

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,7 +2,7 @@
 export default async function Dashborad() {
  
   return (
-    <div>
+    <div className="w-full backdrop-blur-sm max-w-6xl flex justify-center h-[calc(100vh_-_100px)] md:h-[calc(100vh_-_150px)] overflow-auto rounded-md bg-white/50 p-4">
       <h1>Dashboard</h1>
     </div>
   )
